d23a7fe7ca070a5607af3fd88b06a77e912e1b4d
donaldoempire1999 <donaldfotso1999@gmail.com>
2021-07-26 18:07:02 +0100

    Update readme.txt and structure

--- src/routes/publications.ts
+++ src/routes/publications.ts
@@ -20,7 +20,7 @@ router.get('/' , auth,   publicationsCtrl.get_publications_for_current_user);
 router.get('/:id_pub' , auth,   publicationsCtrl.get_publication_for_current_user);
 
 //Afficher une publication particulière pour un id
-router.get('/get/:id_pub' ,  publicationsCtrl.get_publication_by_id);
+router.get('/get/:id_pub' , auth,   publicationsCtrl.get_publication_by_id);
 
 
 
